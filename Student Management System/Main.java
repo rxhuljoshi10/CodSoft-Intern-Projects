8df6be3ce00517114c4284186611bafddf8c06df
Rahul Joshi <joshirahul7040@gmail.com>
2024-07-15 21:07:30 +0530

    Implemented Student Management System class

--- Student Management System/Main.java
+++ Student Management System/Main.java
@@ -1,6 +1,7 @@
 import java.util.*;
+import java.io.*;
 
-class Student{
+class Student implements Serializable{
     int rno;
     String name;
     // long number;
@@ -29,14 +30,53 @@ class Student{
     }
 }
 
+class StudentManagementSystem{
+    private List<Student> studentsList;
+    private final String dataFile = "Student Management System\students.dat";
+    StudentManagementSystem(){
+        studentsList = new ArrayList<>();
+        fetchStudentsData();
+    }
+
+    public void addStudent(Student student){
+        studentsList.add(student);
+        saveStudentsData();
+    }
+
+    public void removeStudent(int rno){
+        studentsList.removeIf(student -> student.getRno() == rno);
+        saveStudentsData();
+    }
+
+    public List<Student> getAllStudents(){
+        return studentsList;
+    }
+
+    public void fetchStudentsData(){
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(dataFile))) {
+            studentsList = (List<Student>) ois.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveStudentsData(){
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(dataFile))){
+            oos.writeObject(studentsList);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
 
 class Main{
     static Scanner scanner;
     public static void main(String[] args){
         scanner = new Scanner(System.in);
+        StudentManagementSystem system = new StudentManagementSystem();
 
         while(true){
-            System.out.print("\033[H\033[2J");
             System.out.println("\n\t*Student Management System*\n");
             System.out.println("1 : Add Student");
             System.out.println("2 : Remove Student");
@@ -48,8 +88,10 @@ class Main{
 
             switch(choice){
                 case 1:
-                    inputStudentDetails();
+                    inputStudentDetails(system);
                     break;
+                case 4:
+                    displayStudents();
                 case 5:
                     return;
                 default:
@@ -59,7 +101,7 @@ class Main{
         }
     }
 
-    private static void inputStudentDetails(){
+    private static void inputStudentDetails(StudentManagementSystem system){
         System.out.print("\033[H\033[2J");
         System.out.println("\tEnter Student Details\n");
         System.out.print("Name : ");
@@ -81,6 +123,11 @@ class Main{
         String grade = scanner.next();
 
         Student student = new Student(rno, name, grade);
+        system.addStudent(student);
         System.out.println("\nSuccessfully Added Student!");
+    }
+
+    public static void displayStudents(){
+
     }
 }
