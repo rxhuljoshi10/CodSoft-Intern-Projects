6b2d6a5c69a08ab75bc5583ed044aaa724ca2e50
Rahul Joshi <joshirahul7040@gmail.com>
2024-07-15 22:32:18 +0530

    version 1

--- Student Management System/Main.java
+++ Student Management System/Main.java
@@ -8,9 +8,9 @@ class Student implements Serializable{
     String grade;
 
     public Student(int rno, String name, String grade){
-        rno = rno;
-        name = name;
-        grade = grade;
+        this.rno = rno;
+        this.name = name;
+        this.grade = grade;
     }
 
     String getName(){ return name; }
@@ -32,7 +32,7 @@ class Student implements Serializable{
 
 class StudentManagementSystem{
     private List<Student> studentsList;
-    private final String dataFile = "Student Management System\students.dat";
+    private final String dataFile = "C:\\Users\\joshi\\Documents\\Programs\\CodSoft-Intern-Projects\\Student Management System\\students.dat";
     StudentManagementSystem(){
         studentsList = new ArrayList<>();
         fetchStudentsData();
@@ -46,6 +46,13 @@ class StudentManagementSystem{
     public void removeStudent(int rno){
         studentsList.removeIf(student -> student.getRno() == rno);
         saveStudentsData();
+    }
+
+    public Student searchStudent(int rno){
+        for(Student student : studentsList){
+            if(student.getRno() == rno) return student;
+        }
+        return null;
     }
 
     public List<Student> getAllStudents(){
@@ -72,9 +79,10 @@ class StudentManagementSystem{
 
 class Main{
     static Scanner scanner;
+    static StudentManagementSystem system;
     public static void main(String[] args){
         scanner = new Scanner(System.in);
-        StudentManagementSystem system = new StudentManagementSystem();
+        system = new StudentManagementSystem();
 
         while(true){
             System.out.println("\n\t*Student Management System*\n");
@@ -88,10 +96,17 @@ class Main{
 
             switch(choice){
                 case 1:
-                    inputStudentDetails(system);
+                    inputStudentDetails();
+                    break;
+                case 2:
+                    removeStudentInput();
+                    break;
+                case 3:
+                    searchStudentInput();
                     break;
                 case 4:
                     displayStudents();
+                    break;
                 case 5:
                     return;
                 default:
@@ -101,7 +116,7 @@ class Main{
         }
     }
 
-    private static void inputStudentDetails(StudentManagementSystem system){
+    private static void inputStudentDetails(){
         System.out.print("\033[H\033[2J");
         System.out.println("\tEnter Student Details\n");
         System.out.print("Name : ");
@@ -127,7 +142,28 @@ class Main{
         System.out.println("\nSuccessfully Added Student!");
     }
 
-    public static void displayStudents(){
+    private static void removeStudentInput(){
+        System.out.print("\nEnter Roll number of student to be removed : ");
+        int rno = scanner.nextInt();
+        system.removeStudent(rno);
+        System.out.println("\nStudent removed successfully!");
+    }
 
+    private static void searchStudentInput(){
+        System.out.print("\nEnter Roll number of student to be removed : ");
+        int rno = scanner.nextInt();
+        Student student = system.searchStudent(rno);
+        if(student != null){
+            System.out.println("\n"+student.name);
+        }
+        else{
+            System.out.println("\nStudent Not Found!");
+        }
+    }
+    public static void displayStudents(){
+        System.out.print("\033[H\033[2J");
+        for(Student student : system.getAllStudents()){
+            System.out.println(student.name);
+        }
     }
 }
